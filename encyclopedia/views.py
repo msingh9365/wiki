85741448bfdec773ba64c9dc9de0f05e7312ad5f
Manish Singh <msingh9365@gmail.com>
2020-08-01 23:21:59 +0530

    Entry Page Specification done :)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request,title):
+    file = util.get_entry(title)
+    if file is None:
+        return render(request,"encyclopedia/error.html",{
+            "title":title.capitalize()
+        })
+
+    html = markdown2.markdown(file)
+
+    return render(request,"encyclopedia/title.html",{
+        "title" : title.capitalize(),
+        "content" : html
+    })
+    
+    
 
