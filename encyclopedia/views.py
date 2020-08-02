2235f04c08a20753b2d59ff84ffe68b730f8b65e
Manish Singh <msingh9365@gmail.com>
2020-08-02 18:13:17 +0530

    3rd Specification:Search done :)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import re
 
 from . import util
 import markdown2
@@ -7,6 +12,22 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def search(request):
+    if request.method == "GET":
+        q = request.GET["q"]
+        if util.get_entry(q) is not None:
+            return HttpResponseRedirect(reverse('entry',args=(q,)))
+        
+        result=[]
+        for title in util.list_entries():
+            if q.upper() in title.upper():
+                result.append(title)
+        return render(request,"encyclopedia/search.html",{
+            "matches" : result,
+            "q" : q
+        })
+
 
 def entry(request,title):
     file = util.get_entry(title)
